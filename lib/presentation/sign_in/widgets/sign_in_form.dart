86d9c90e0399afdbf1d0a5f8a98e7b229ffcfb33
Tocut Andrei <andrei.tocut1@gmail.com>
2023-01-04 09:23:16 +0200

    started presentation layer

---
+++ lib/presentation/sign_in/widgets/sign_in_form.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:starter_template/application/auth/sign_in_form/sign_in_form_bloc.dart';
+
+class SignInForm extends StatelessWidget {
+  const SignInForm({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text("asa");
+  }
+}
